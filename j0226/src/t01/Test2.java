42019e8bbf0dd441a3d2e50704181c7c354ccf2d
seeun123456 <hwaetig@naver.com>
2024-02-27 11:01:20 +0900

    first project

---
+++ j0226/src/t01/Test2.java
@@ -0,0 +1,7 @@
+package t01;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+	}
+}
---
+++ j0226/src/t01/Test4.java
@@ -0,0 +1,19 @@
+package t01;
+
+public class Test4 {
+	// main메소드
+	public static void main(String[] args) {//자바의 생명은 타입 String=문자열,args=변수 , 메소드블럭 
+		//콘솔에 내용 출력 : sysout
+		System.out.println("1.반갑\t습니다.");// \t는 탭, \n은 엔터키(줄바꾸기) 
+		System.out.print("2.반갑습니다.\n\n"); 
+		System.out.println("3.반갑습니다."); 
+		
+		/*
+		 이곳은 여러줄의 주석처리
+		 여러줄에 설명을 기술할 수 있습니다.
+		 
+		 */
+		System.out.println("4.반갑습니다."); 
+		
+	}
+}
